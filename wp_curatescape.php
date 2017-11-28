12c57ed01dcde7d904a96b41868429b468df64b1
Erin Bell <orchard@Erins-MacBook-Pro.local>
2017-11-28 11:13:41 -0500

    updates plugin header

--- wp_curatescape.php
+++ wp_curatescape.php
@@ -8,7 +8,14 @@ Text Domain: wp_curatescape
 Domain Path: /languages
 Author: CSU Center for Public History + Digital Humanities 
 Author URI: http://csudigitalhumanities.org
-License: GPL2
+Contributors: ebellempire,cphdh
+Donate link: https://csudigitalhumanities.org/about/donate/
+Tags: curatescape,history,public history, digital humanities
+Requires at least: 4.9
+Tested up to: 4.9
+Requires PHP: 5.2.4
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
 /*  
