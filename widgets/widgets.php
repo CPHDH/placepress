53b4d0bed70b4e93dff6aae280cfc7b5197ec581
Erin Bell <orchard@Erins-MacBook-Pro.local>
2017-12-19 13:37:25 -0500

    adds stories/tours widget

--- widgets/widgets.php
+++ widgets/widgets.php
@@ -4,7 +4,7 @@ if( !defined('ABSPATH') ){
 }	
 
 /**
- * Adds Foo_Widget widget.
+ * Adds Curatescape_Widget widget.
  */
 class Curatescape_Widget extends WP_Widget {
 
@@ -13,8 +13,8 @@ class Curatescape_Widget extends WP_Widget {
 	 */
 	function __construct() {
 		parent::__construct(
-			'foo_widget', // Base ID
-			esc_html__( 'Curatescape Test Widget', 'wp_curatescape' ), // Name
+			'curatescape_widget', // Base ID
+			esc_html__( 'Curatescape Widget', 'wp_curatescape' ), // Name
 			array( 'description' => esc_html__( 'A Curatescape Widget', 'wp_curatescape' ), ) // Args
 		);
 	}
@@ -28,11 +28,19 @@ class Curatescape_Widget extends WP_Widget {
 	 * @param array $instance Saved values from database.
 	 */
 	public function widget( $args, $instance ) {
+		$p=get_posts(array(
+			'post_type'=>$instance['type'],
+			'numberposts'=>$instance['number'],
+		));		
 		echo $args['before_widget'];
 		if ( ! empty( $instance['title'] ) ) {
 			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
 		}
-		echo esc_html__( 'Hello, World!', 'wp_curatescape' );
+		echo '<ul>';
+		foreach($p as $post){
+			echo '<li><a href="'.get_permalink( $post ).'">'.$post->post_title.'</a></li>';
+		}
+		echo '</ul>';
 		echo $args['after_widget'];
 	}
 
@@ -44,12 +52,29 @@ class Curatescape_Widget extends WP_Widget {
 	 * @param array $instance Previously saved values from database.
 	 */
 	public function form( $instance ) {
-		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'New title', 'wp_curatescape' );
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'Curatescape', 'wp_curatescape' );
+		$number = ! empty( $instance['number'] ) ? $instance['number'] : 3;
+		$type = ! empty( $instance['type'] ) ? $instance['type'] : 'stories';
+		$select_options = array('stories'=>esc_html__('Stories','wp_curatescape'),'tours'=>esc_html__('Tours','wp_curatescape'))
 		?>
 		<p>
-		<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'wp_curatescape' ); ?></label> 
-		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+			<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'wp_curatescape' ); ?></label> 
+			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
 		</p>
+		<p>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'type' ) ); ?>"><?php esc_attr_e( 'Type:', 'wp_curatescape' ); ?></label>	
+			<select name="<?php echo esc_attr( $this->get_field_name( 'type' ) ); ?>">';
+				<?php foreach($select_options as $value=>$option){
+					$selected = selected( $type === $value,true,false );
+					echo '<option value="'.$value.'" '.$selected.'>'.$option.'</option>';
+				}?>
+			</select>
+		</p>
+		<p>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'number' ) ); ?>"><?php esc_attr_e( 'Number to show:', 'wp_curatescape' ); ?></label> 
+			<input id="<?php echo esc_attr( $this->get_field_id( 'number' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'number' ) ); ?>" type="number" min="1" max="10" value="<?php echo esc_attr( $number ); ?>">	
+		</p>
+
 		<?php 
 	}
 
@@ -66,10 +91,10 @@ class Curatescape_Widget extends WP_Widget {
 	public function update( $new_instance, $old_instance ) {
 		$instance = array();
 		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-
+		$instance['number'] = ( ! empty( $new_instance['number'] ) ) ? strip_tags($new_instance['number']) : 3;
+		$instance['type'] = ( ! empty( $new_instance['type'] ) ) ? strip_tags($new_instance['type']) : 'stories';
 		return $instance;
 	}
-
 }
 
 function register_curatescape_widget() {
