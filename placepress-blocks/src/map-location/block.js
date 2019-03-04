604bb98fc8e1c031979a7952bdc37ca87df289de
Erin Bell <ebellempire@gmail.com>
2019-03-04 12:59:13 -0500

    …working

--- placepress-blocks/src/map-location/block.js
+++ placepress-blocks/src/map-location/block.js
@@ -9,8 +9,9 @@
 import './style.scss';
 import './editor.scss';
 
-const { __ } = wp.i18n; // Import __() from wp.i18n
-const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
+const { __ } = wp.i18n;
+const { registerBlockType, getBlockDefaultClassName } = wp.blocks;
+const { PlainText, InspectorControls } = wp.editor;
 
 /**
  * Register: aa Gutenberg Block.
@@ -35,59 +36,48 @@ registerBlockType( 'placepress/block-map-location', {
     __( 'Location' ),
  		__( 'PlacePress' ),
  	],
-
-	/**
-	 * The edit function describes the structure of your block in the context of the editor.
-	 * This represents what the editor will render when the block is used.
-	 *
-	 * The "edit" property must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 */
+  supports:{
+    anchor:true,
+    html: false,
+    multiple: false,
+    reusable: false,
+  },
+  description: __( 'A block for adding a location map.' ),
+  attributes: {
+    content: {
+        type: 'string',
+        selector: '.map-location-pp',
+      }
+  },
 	edit: function( props ) {
-		// Creates a <p class='wp-block-cgb-block-placepress-blocks'></p>.
+    const { className, setAttributes } = props;
+    const { attributes } = props;
+
+    function changeContent(changes) {
+        setAttributes({
+            content: changes
+        })
+    }
 		return (
 			<div className={ props.className }>
-				<p>— Hello from the backend.</p>
-				<p>
-					CGB BLOCK: <code>placepress-blocks</code> is a new Gutenberg block
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
+      <PlainText
+          className="map-location-pp"
+          tagName="p"
+          placeholder={ __("Enter coordinates here.", 'wp_placepress') }
+          value={attributes.content}
+          onChange={changeContent}
+          />
 			</div>
 		);
 	},
-
-	/**
-	 * The save function defines the way in which the different attributes should be combined
-	 * into the final markup, which is then serialized by Gutenberg into post_content.
-	 *
-	 * The "save" property must be specified and must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 */
 	save: function( props ) {
+    const className = getBlockDefaultClassName('placepress/block-map-location');
+    const { attributes } = props;
+
 		return (
-			<div>
-				<p>— Hello from the frontend.</p>
-				<p>
-					CGB BLOCK: <code>placepress-blocks</code> is a new Gutenberg block.
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
-			</div>
+      <div className={className}>
+      <p class="map-location-pp">{attributes.content}</p>
+      </div>
 		);
 	},
 } );
