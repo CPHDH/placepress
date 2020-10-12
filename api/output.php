1172a4a24d4495a1b82595dba8023be51e547201
Erin Bell <ebellempire@gmail.com>
2020-10-12 17:29:15 -0400

    adds location types to API output

--- api/output.php
+++ api/output.php
@@ -18,6 +18,15 @@ function placepress_delete_transients() {
      delete_transient( 'placepress_locations_public' );
 }
 
+function placepress_location_type_array($postID){
+	$types = get_the_terms( $postID, 'location_types' );
+	$slugs=[];
+	foreach ($types as $type) {
+		$slugs[]=$type->slug;
+	}
+	return $slugs;
+}
+
 // LOCATIONS PUBLIC JSON
 function placepress_render_public_locations_json(){
 	if ( false === ( $output = get_transient( 'placepress_locations' ) ) ) {
@@ -39,6 +48,7 @@ function placepress_render_public_locations_json(){
             'permalink' => get_permalink( $post->ID ),
   		    	'api_coordinates_pp' => $postMeta['api_coordinates_pp'][0],
   		    	'thumbnail'=>get_the_post_thumbnail_url($post,'medium'),
+						'type'=>placepress_location_type_array($post->ID)
   		    );
 		  }
 	    set_transient( 'placepress_locations_public', $output, 3 * MINUTE_IN_SECONDS ); // cache results
