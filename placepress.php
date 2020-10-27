54af613307509f8ff271c7d14dbe36005dab5ab3
Erin Bell <ebellempire@gmail.com>
2020-10-27 16:24:38 -0400

    tested up to ++

--- placepress.php
+++ placepress.php
@@ -12,7 +12,7 @@ Contributors: ebellempire,cphdh
 Donate link: https://csudigitalhumanities.org/about/donate/
 Tags: placepress, history, public history, digital humanities, maps, curatescape, blocks, gutenburg, location, tour, post types
 Requires at least: 5.0
-Tested up to: 5.2.1
+Tested up to: 5.5.1
 Requires PHP: 5.2.4
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
