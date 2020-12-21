2985985424db88836129c5fef067e6b926385903
Erin Bell <ebellempire@gmail.com>
2020-12-21 17:04:51 -0500

    adds user control for location type selection

--- placepress-blocks/src/placepress.js
+++ placepress-blocks/src/placepress.js
@@ -88,14 +88,62 @@ document.addEventListener("DOMContentLoaded", () => {
 			return path[2] || false; // example.com/location-types/cities => returns cities
 		};
 
+		const addMarkers = (map, data, markers, location_type) => {
+			data
+				.filter((data) =>
+					location_type ? data.type.includes(location_type) : data
+				)
+				.forEach(function (post) {
+					const coords = post.api_coordinates_pp.split(",");
+					if (coords.length == 2) {
+						const marker = L.marker(coords, {
+							id: post.id,
+							title: post.title,
+							permalink: post.permalink,
+							coords: coords,
+							thumbnail: post.thumbnail,
+						});
+						// user actions: CLICK
+						marker.on("click", function (e) {
+							const popup = L.popup().setContent(
+								'<a href="' +
+									e.target.options.permalink +
+									'" class="map-thumb" style="background-image:linear-gradient(to bottom,rgba(0,0,0,0) 50%,rgba(0,0,0,0.7) 70%,rgba(0,0,0,1) 100%),url(' +
+									e.target.options.thumbnail +
+									')">' +
+									'<span class="map-title" href="' +
+									e.target.options.permalink +
+									'">' +
+									e.target.options.title +
+									"</span>" +
+									"</a>"
+							);
+							e.target.unbindPopup().bindPopup(popup).openPopup();
+						});
+						markers.push(marker);
+
+						// vertical center on popup open
+						map.on("popupopen", function (e) {
+							const px = map.project(e.popup._latlng);
+							px.y -= e.popup._container.clientHeight / 2;
+							map.panTo(map.unproject(px), { animate: true });
+						});
+					}
+				});
+			if (typeof L.markerClusterGroup === "function") {
+				const clusterGroup = L.markerClusterGroup();
+				clusterGroup.addLayers(markers).addTo(map);
+				map.fitBounds(clusterGroup.getBounds(), { padding: [50, 50] });
+			} else {
+				const markersGroup = L.featureGroup(markers).addTo(map);
+				map.fitBounds(markersGroup.getBounds(), { padding: [50, 50] });
+			}
+		};
+
 		// API XMLHttpRequest
-		const addGlobalMarkersViaAPI = (
-			map,
-			markersLayer,
-			isArchive = false,
-			locationType = null
-		) => {
-			const location_type = locationType || getArchiveLocationType();
+		const buildGlobalMapUI = (map, settings, tileSets) => {
+			const markers = [];
+			const location_type = settings.locationType || getArchiveLocationType();
 			const locations_json =
 				location.protocol +
 				"//" +
@@ -107,57 +155,16 @@ document.addEventListener("DOMContentLoaded", () => {
 				if (request.status >= 200 && request.status < 400) {
 					let data = JSON.parse(this.response);
 					if (typeof data !== "undefined") {
-						data
-							.filter((data) =>
-								isArchive && location_type
-									? data.type.includes(location_type)
-									: data
-							)
-							.forEach((post) => {
-								const coords = post.api_coordinates_pp.split(",");
-								if (coords.length == 2) {
-									const marker = L.marker(coords, {
-										id: post.id,
-										title: post.title,
-										permalink: post.permalink,
-										coords: coords,
-										thumbnail: post.thumbnail,
-									});
-									// user actions: CLICK
-									marker.on("click", (e) => {
-										const popup = L.popup().setContent(
-											'<a href="' +
-												e.target.options.permalink +
-												'" class="map-thumb" style="background-image:linear-gradient(to bottom,rgba(0,0,0,0) 50%,rgba(0,0,0,0.7) 70%,rgba(0,0,0,1) 100%),url(' +
-												e.target.options.thumbnail +
-												')">' +
-												'<span class="map-title" href="' +
-												e.target.options.permalink +
-												'">' +
-												e.target.options.title +
-												"</span>" +
-												"</a>"
-										);
-										e.target.unbindPopup().bindPopup(popup).openPopup();
-									});
-									markersLayer.push(marker);
+						// Add Markers
+						addMarkers(map, data, markers, location_type);
 
-									// vertical center on popup open
-									map.on("popupopen", (e) => {
-										const px = map.project(e.popup._latlng);
-										px.y -= e.popup._container.clientHeight / 2;
-										map.panTo(map.unproject(px), { animate: true });
-									});
-								}
-							});
-						if (typeof L.markerClusterGroup === "function") {
-							const clusterGroup = L.markerClusterGroup();
-							clusterGroup.addLayers(markersLayer).addTo(map);
-							map.fitBounds(clusterGroup.getBounds(), { padding: [50, 50] });
-						} else {
-							const markersGroup = L.featureGroup(markersLayer).addTo(map);
-							map.fitBounds(markersGroup.getBounds(), { padding: [50, 50] });
+						if (settings.locationTypeSelection == "true") {
+							// controls/user actions: SELECT Location Type
+							locationTypeUserSelectControls(map, data, location_type);
 						}
+
+						// additional controls
+						addAdditionalControls(tileSets, map);
 					} else {
 						console.warn(
 							"PlacePress: Your request did not return any Locations. Please ensure that you have Location posts that use the PlacePress Location Map block."
@@ -170,6 +177,78 @@ document.addEventListener("DOMContentLoaded", () => {
 				}
 			};
 			request.send();
+		};
+
+		// User Type Select Control
+		const locationTypeUserSelectControls = (map, data, location_type) => {
+			// Get Types/Categories
+			let select_options = [];
+			data.forEach((d) => {
+				d.type.forEach((t) => {
+					select_options.push(t);
+				});
+			});
+			select_options = [...new Set(select_options)];
+			select_options.sort();
+			L.Control.CategorySelect = L.Control.extend({
+				onAdd: function (map) {
+					const container = L.DomUtil.create("div", "category-select-pp");
+					const select = L.DomUtil.create(
+						"select",
+						"select-pp leaflet-touch",
+						container
+					);
+					select.tabindex = "0";
+
+					// Default selection: all
+					const option = L.DomUtil.create("option", "option-pp", select);
+					option.setAttribute("value", "");
+					option.innerHTML = defaults.all_location_types_label;
+
+					// Location Type selections
+					let add_opt = null;
+					select_options.forEach((opt) => {
+						add_opt = L.DomUtil.create("option", "editor-option-pp", select);
+
+						if (location_type === opt) {
+							add_opt.setAttribute("selected", "");
+						}
+
+						let name = opt
+							.split("-")
+							.map((word) => {
+								return word[0].toUpperCase() + word.substr(1);
+							})
+							.join(" ");
+						add_opt.setAttribute("value", opt);
+						add_opt.innerHTML = name;
+					});
+
+					// on new type selection...
+					select.onchange = (e) => {
+						let selected_type = e.target.selectedOptions[0].value;
+
+						// remove current markers
+						map.eachLayer((layer) => {
+							if (typeof layer._featureGroup !== "undefined") {
+								map.removeLayer(layer);
+							}
+						});
+						// add selected markers
+						addMarkers(map, data, [], selected_type);
+					};
+
+					return container;
+				},
+
+				onRemove: function () {
+					// Nothing to do here
+				},
+			});
+			L.control.categoryselect = function (opts) {
+				return new L.Control.CategorySelect(opts);
+			};
+			L.control.categoryselect({ position: "topright" }).addTo(map);
 		};
 
 		// Fit Bounds Control
@@ -490,11 +569,8 @@ document.addEventListener("DOMContentLoaded", () => {
 				map.scrollWheelZoom.enable();
 			});
 
-			// controls
-			addAdditionalControls(tileSets, map);
-
-			// add location markers
-			addGlobalMarkersViaAPI(map, markersLayer, isArchive, s.locationType);
+			// API fetch, add location markers, controls, etc
+			buildGlobalMapUI(map, s, tileSets);
 		};
 
 		// MAIN
