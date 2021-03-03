0f48d8eca2f5e202c61aef9698cf58dd77390360
Erin Bell <ebellempire@gmail.com>
2021-03-03 16:08:19 -0500

    formatting

--- placepress.php
+++ placepress.php
@@ -35,102 +35,123 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 Copyright 2019  CSU Center for Public History + Digital Humanities  (email : digitalhumanities@csuohio.edu)
 */
 
-if (! defined('ABSPATH')) {
-    exit;
+if (!defined("ABSPATH")) {
+  exit();
 }
 
 // Constants
-define('PRECONNECT_CDN', 'https://unpkg.com');
-define('LEAFLET_JS', 'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js');
-define('LEAFLET_CSS', 'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css');
-define('CLUSTER_JS', 'https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js');
-define('CLUSTER_CSS', 'https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.css');
-define('CLUSTER_CSS_DEFAULT', 'https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.Default.css');
-define('PLACEPRESS_TILE_PROVIDER', plugins_url() . '/placepress/placepress-blocks/src/tile-provider.js');
-define('PLACEPRESS_MAIN_JS', plugins_url() . '/placepress/placepress-blocks/src/placepress.js');
-define('PLACEPRESS_SETTINGS_JS', plugins_url() .'/placepress/admin/settings/settings.js');
-define('PLACEPRESS_SETTINGS_CSS', plugins_url() .'/placepress/admin/settings/settings.css');
+define("PRECONNECT_CDN", "https://unpkg.com");
+define("LEAFLET_JS", "https://unpkg.com/leaflet@1.7.1/dist/leaflet.js");
+define("LEAFLET_CSS", "https://unpkg.com/leaflet@1.7.1/dist/leaflet.css");
+define(
+  "CLUSTER_JS",
+  "https://unpkg.com/leaflet.markercluster@1.4.1/dist/leaflet.markercluster.js"
+);
+define(
+  "CLUSTER_CSS",
+  "https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.css"
+);
+define(
+  "CLUSTER_CSS_DEFAULT",
+  "https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.Default.css"
+);
+define(
+  "PLACEPRESS_TILE_PROVIDER",
+  plugins_url() . "/placepress/placepress-blocks/src/tile-provider.js"
+);
+define(
+  "PLACEPRESS_MAIN_JS",
+  plugins_url() . "/placepress/placepress-blocks/src/placepress.js"
+);
+define(
+  "PLACEPRESS_SETTINGS_JS",
+  plugins_url() . "/placepress/admin/settings/settings.js"
+);
+define(
+  "PLACEPRESS_SETTINGS_CSS",
+  plugins_url() . "/placepress/admin/settings/settings.css"
+);
 
 /*
-** PLUGIN SETTINGS
-*/
-require_once plugin_dir_path(__FILE__). 'admin/settings/settings.php';
+ ** PLUGIN SETTINGS
+ */
+require_once plugin_dir_path(__FILE__) . "admin/settings/settings.php";
 
 /*
-** POST TYPES AND TAXONOMIES
-*/
-require_once plugin_dir_path(__FILE__). 'admin/post_types.php';
+ ** POST TYPES AND TAXONOMIES
+ */
+require_once plugin_dir_path(__FILE__) . "admin/post_types.php";
 
 /*
-** METABOXES
-*/
-require_once plugin_dir_path(__FILE__). 'admin/metaboxes.php';
+ ** METABOXES
+ */
+require_once plugin_dir_path(__FILE__) . "admin/metaboxes.php";
 
 /*
-** BLOCKS
-*/
-require_once plugin_dir_path(__FILE__). 'placepress-blocks/src/init.php';
+ ** BLOCKS
+ */
+require_once plugin_dir_path(__FILE__) . "placepress-blocks/src/init.php";
 
 /*
-** WIDGETS
-*/
-require_once plugin_dir_path(__FILE__). 'widgets/widgets.php';
+ ** WIDGETS
+ */
+require_once plugin_dir_path(__FILE__) . "widgets/widgets.php";
 
 /*
-** MENUS
-*/
-require_once plugin_dir_path(__FILE__). 'admin/menus.php';
+ ** MENUS
+ */
+require_once plugin_dir_path(__FILE__) . "admin/menus.php";
 
 /*
-** DASHBOARD
-*/
-require_once plugin_dir_path(__FILE__). 'admin/dashboard.php';
+ ** DASHBOARD
+ */
+require_once plugin_dir_path(__FILE__) . "admin/dashboard.php";
 
 /*
-** LANGUAGES
-*/
-require_once plugin_dir_path(__FILE__). 'languages/languages.php';
+ ** LANGUAGES
+ */
+require_once plugin_dir_path(__FILE__) . "languages/languages.php";
 
 /*
-** HELPERS
-*/
-require_once plugin_dir_path(__FILE__). 'helpers/helpers.php';
+ ** HELPERS
+ */
+require_once plugin_dir_path(__FILE__) . "helpers/helpers.php";
 
 /*
-** API
-*/
-require_once plugin_dir_path(__FILE__). 'api/output.php';
+ ** API
+ */
+require_once plugin_dir_path(__FILE__) . "api/output.php";
 
 /*
-** FILTERS
-*/
-require_once plugin_dir_path(__FILE__). 'filters/filters.php';
+ ** FILTERS
+ */
+require_once plugin_dir_path(__FILE__) . "filters/filters.php";
 
 /*
-** ACTIVATE
-*/
+ ** ACTIVATE
+ */
 function placepress_activate()
 {
-    add_option('placepress_do_activation_redirect', true);
+  add_option("placepress_do_activation_redirect", true);
 }
-register_activation_hook(__FILE__, 'placepress_activate');
+register_activation_hook(__FILE__, "placepress_activate");
 
 function placepress_plugin_activation_redirect()
 {
-    if (get_option('placepress_do_activation_redirect', false)) {
-        delete_option('placepress_do_activation_redirect');
-        flush_rewrite_rules();
-        wp_redirect("options-general.php?page=placepress");
-        exit;
-    }
+  if (get_option("placepress_do_activation_redirect", false)) {
+    delete_option("placepress_do_activation_redirect");
+    flush_rewrite_rules();
+    wp_redirect("options-general.php?page=placepress");
+    exit();
+  }
 }
-add_action('admin_init', 'placepress_plugin_activation_redirect');
+add_action("admin_init", "placepress_plugin_activation_redirect");
 
 /*
-** DEACTIVATE
-*/
+ ** DEACTIVATE
+ */
 function placepress_deactivate()
 {
-    flush_rewrite_rules();
+  flush_rewrite_rules();
 }
-register_deactivation_hook(__FILE__, 'placepress_deactivate');
+register_deactivation_hook(__FILE__, "placepress_deactivate");
